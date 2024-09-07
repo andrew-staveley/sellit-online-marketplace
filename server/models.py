c00aa3038f2423f87d3fe188801afdb79799692c
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-09-06 21:18:38 -0400

    Created 'user' model.

--- server/models.py
+++ server/models.py
@@ -1,6 +1,32 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    serialize_rules = ('-listings', '-_password_hash')
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique='True')
+    _password_hash = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    bio = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.column(db.String, nullable=False)
+    reviews = db.relationship('Review', backref='user')
+    listings = db.relationship('Listing', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self,_password_hash, password.encode('utf-8'))
