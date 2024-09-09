04f2c744b4616ccc7479444ca297205ed4f66400
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-09-09 17:55:23 -0400

    Small Chnages

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,9 @@ function Header({ session }) {
     return (
         <header className="header">
             <NavLink to="/" className="button" style={{float: 'left'}}>Sellit</NavLink>
-            <NavLink to="/search" className="button" style={{float: 'right'}}>Search</NavLink>
+            {session !== null ? <NavLink to="/account" className="button" style={{float: 'right'}}>Account</NavLink> : <NavLink to="/login" className="button" style={{float: 'right'}}>Login</NavLink>}
             <NavLink to="/newlisting" className="button" style={{float: 'right'}}>New</NavLink>
-            {session != null ? <NavLink to="/account" className="button" style={{float: 'right'}}>Account</NavLink> : <NavLink to="/login" className="button" style={{float: 'right'}}>Login</NavLink>}
+            <NavLink to="/search" className="button" style={{float: 'right'}}>Search</NavLink>
         </header>
     )
 }
