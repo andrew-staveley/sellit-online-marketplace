2a6600bd7d55fbe0cb6d3e1304328c603540bd97
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-09-09 17:51:38 -0400

    Added nav bar buttons/links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom"
 
-function Header() {
+function Header({ session }) {
     return (
-        <h1>Header</h1>
+        <header className="header">
+            <NavLink to="/" className="button" style={{float: 'left'}}>Sellit</NavLink>
+            <NavLink to="/search" className="button" style={{float: 'right'}}>Search</NavLink>
+            <NavLink to="/newlisting" className="button" style={{float: 'right'}}>New</NavLink>
+            {session != null ? <NavLink to="/account" className="button" style={{float: 'right'}}>Account</NavLink> : <NavLink to="/login" className="button" style={{float: 'right'}}>Login</NavLink>}
+        </header>
     )
 }
 
