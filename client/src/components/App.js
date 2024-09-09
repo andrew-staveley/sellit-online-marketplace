d0a66c0e02a9608b4095f0efdc2bb6d7293843a8
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-09-09 13:44:22 -0400

    Made inital commits for component files, added imports into App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Routes } from "react-router-dom";
+import Header from "./Header.js"
+import HomePage from "./HomePage.js"
+import LoginPage from "./LoginPage.js"
+import SignUpPage from "./SignUpPage.js"
+import Account from "./Account.js"
+import UserPage from "./UserPage.js"
+import ListingPage from "./ListingPage.js"
+import SearchPage from "./SearchPage.js"
+import NewListing from "./ListingPage.js"
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <>
+      <main>
+        <Header />
+          <Routes>
+            <Route path="/" element={ <HomePage />}/>
+            <Route path="/login" element={ <LoginPage />}/>
+            <Route path='/signup' element={ <SignUpPage />}/>
+            <Route path='/account' element={ <Account />}/>
+            <Route path='/user/:id' element={ <UserPage />}/>
+            <Route path='/listing/:id' element={ <ListingPage />}/>
+            <Route path='/search' element={ <SearchPage />}/>
+            <Route path='/newlisting' element={ <NewListing />}/>
+          </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
